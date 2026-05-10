be58395a038211ad929b04c03362b8383e904766
Emanuel Leal <emaleavil@gmail.com>
2026-05-10 15:53:55 +0200

    feat: ✨ Add compose in login module

--- features/login/build.gradle.kts
+++ features/login/build.gradle.kts
@@ -4,17 +4,35 @@ plugins {
             .get()
             .pluginId,
     )
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.eeema.android.login"
+
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+
     testImplementation(libs.junit)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
