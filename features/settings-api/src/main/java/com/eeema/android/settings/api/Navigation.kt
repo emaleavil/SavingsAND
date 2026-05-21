15eb2a3c493a5f73a80c255cfe0aa9a893770fad
Emanuel Leal <emaleavil@gmail.com>
2026-05-21 17:00:55 +0200

    feat: ✨Add settings-api module for navigation

---
+++ features/settings-api/src/main/java/com/eeema/android/settings/api/Navigation.kt
@@ -0,0 +1,9 @@
+@file:Suppress("ktlint:standard:filename")
+
+package com.eeema.android.settings.api
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Settings : NavKey
