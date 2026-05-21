cb2cfcde3dd11218e22e2ecd0b3297a3ab05e045
Emanuel Leal <emaleavil@gmail.com>
2026-05-21 17:00:25 +0200

    feat: ✨Add add-api module for navigation

---
+++ features/add-api/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id(
+        libs.plugins.custom.android.library
+            .get()
+            .pluginId,
+    )
+    alias(libs.plugins.jetbrains.kotlin.serialization)
+}
+
+android {
+    namespace = "com.eeema.android.add.api"
+}
+
+dependencies {
+    implementation(libs.androidx.navigation3.runtime)
+}
---
+++ features/add-api/src/main/java/com/eeema/android/add/api/Navigation.kt
@@ -0,0 +1,9 @@
+@file:Suppress("ktlint:standard:filename")
+
+package com.eeema.android.add.api
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Add : NavKey
