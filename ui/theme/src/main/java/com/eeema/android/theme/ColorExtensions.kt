21dbf3470407dcdd2ead5f9140c86e2ae17bed2a
Emanuel Leal <emaleavil@gmail.com>
2026-06-12 08:19:29 +0200

    feat: ✨ Add basic theme configuration

---
+++ ui/theme/src/main/java/com/eeema/android/theme/ColorExtensions.kt
@@ -0,0 +1,7 @@
+package com.eeema.android.theme
+
+import androidx.compose.material3.ColorScheme
+import androidx.compose.ui.graphics.Color
+
+val ColorScheme.placeholder: Color
+    get() = PlaceholderColor
---
+++ ui/theme/src/main/java/com/eeema/android/theme/Shapes.kt
@@ -0,0 +1,11 @@
+package com.eeema.android.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes =
+    Shapes(
+        extraLarge = RoundedCornerShape(32.0.dp),
+        large = RoundedCornerShape(24.0.dp),
+    )
